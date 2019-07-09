581d6d7c6bf2542ab5409fe28398190cfa606ce6
hunkel <kolan19988@yandex.ru>
2019-07-09 23:17:38 +0300

    add database object

---
+++ app/src/main/java/ru/hunkel/mgrouptracker/database/TrackingDatabase.kt
@@ -0,0 +1,37 @@
+package ru.hunkel.mgrouptracker.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import kotlinx.coroutines.runBlocking
+import ru.hunkel.mgrouptracker.database.dao.TrackingDao
+import ru.hunkel.mgrouptracker.database.entities.Event
+import ru.hunkel.mgrouptracker.database.entities.GPSPoint
+import ru.hunkel.mgrouptracker.database.entities.Punches
+
+@Database(
+    entities = [Event::class, GPSPoint::class, Punches::class],
+    version = 1
+)
+abstract class TrackingDatabase : RoomDatabase() {
+    abstract fun trackingModel(): TrackingDao
+
+    companion object {
+        private var INSTANCE: TrackingDatabase? = null
+
+        fun getInstance(context: Context): TrackingDatabase {
+            if (INSTANCE == null) {
+                runBlocking {
+                    INSTANCE = Room.databaseBuilder(
+                        context.applicationContext,
+                        TrackingDatabase::class.java,
+                        "tracking-database"
+                    )
+                        .build()
+                }
+            }
+            return INSTANCE!!
+        }
+    }
+}
