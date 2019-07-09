6e2708f2ef4fefe6888ba9ad5a30bde69a1ec13d
hunkel <kolan19988@yandex.ru>
2019-07-09 23:31:50 +0300

    add base structure of db manager

--- app/src/main/java/ru/hunkel/mgrouptracker/database/utils/DatabaseManager.kt
+++ app/src/main/java/ru/hunkel/mgrouptracker/database/utils/DatabaseManager.kt
@@ -1,6 +1,19 @@
 package ru.hunkel.mgrouptracker.database.utils
 
+import android.content.Context
+import ru.hunkel.mgrouptracker.database.TrackingDatabase
+import ru.hunkel.mgrouptracker.database.entities.Punches
+
 
 object DatabaseManager {
+    private var mDatabase: TrackingDatabase? = null
 
+    fun insertPunches(context: Context, punch: Punches) {
+        connectToDatabase(context)
+        mDatabase!!.trackingModel().insertPunch(punch)
+    }
+
+    private fun connectToDatabase(context: Context) {
+        if (mDatabase == null) mDatabase = TrackingDatabase.getInstance(context)
+    }
 }
