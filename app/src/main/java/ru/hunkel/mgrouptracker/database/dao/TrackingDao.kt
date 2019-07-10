ea4034d8f562a9f750fc408c2a755b55f7a8abfd
hunkel <kolan19988@yandex.ru>
2019-07-10 22:22:04 +0300

    add start/stop event functionality

--- app/src/main/java/ru/hunkel/mgrouptracker/database/dao/TrackingDao.kt
+++ app/src/main/java/ru/hunkel/mgrouptracker/database/dao/TrackingDao.kt
@@ -3,6 +3,7 @@ package ru.hunkel.mgrouptracker.database.dao
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
 import ru.hunkel.mgrouptracker.database.entities.Event
 
 /**
@@ -14,6 +15,12 @@ interface TrackingDao {
     @Insert
     fun addEvent(event: Event)
 
+    @Update
+    fun updateEvent(event: Event)
+
     @Query("SELECT * FROM events WHERE id = :id")
     fun getEventById(id: Int): Event
+
+    @Query("SELECT * FROM events ORDER BY ID DESC LIMIT 1")
+    fun getLastEvent():Event
 }
--- app/src/main/java/ru/hunkel/mgrouptracker/database/utils/DatabaseManager.kt
+++ app/src/main/java/ru/hunkel/mgrouptracker/database/utils/DatabaseManager.kt
@@ -1,9 +1,52 @@
 package ru.hunkel.mgrouptracker.database.utils
 
 import android.content.Context
+import android.util.Log
 import ru.hunkel.mgrouptracker.database.TrackingDatabase
+import ru.hunkel.mgrouptracker.database.entities.Event
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class DatabaseManager(context: Context) {
     private var mDb = TrackingDatabase.getInstance(context)
+    private val TAG = "DB-Manager"
+
+    private var mCurrentEvent: Event = Event()
+
+    fun actionStartEvent() {
+        val event = Event(
+            startTime = System.currentTimeMillis()
+        )
+        Log.i(TAG, "event started")
+        mCurrentEvent = event
+        mDb.trackingModel().addEvent(mCurrentEvent)
+    }
+
+    fun actionStopEvent() {
+        val event = mDb.trackingModel().getLastEvent()
+        event.endTime = System.currentTimeMillis()
+        mDb.trackingModel().updateEvent(event)
+        Log.i(TAG, "event stopped")
+        actionGetLastEvent()
+
+    }
+
+    fun actionGetLastEvent() {
+        val event = mDb.trackingModel().getLastEvent()
+
+        Log.i(
+            TAG,
+            "EVENT INFO:\n\t" +
+                    "id: ${event.id}\n\t" +
+                    "start time: ${convertLongToTime(event.startTime)}\n\t" +
+                    "end time: ${convertLongToTime(event.endTime)}\n"
+        )
+    }
+
+    fun convertLongToTime(time: Long): String {
+        val date = Date(time)
+        val format = SimpleDateFormat("yyyy.MM.dd HH:mm:ss")
+        return format.format(date)
+    }
 }
