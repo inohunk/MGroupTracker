db86b7d21cfa362bb3320963cfdecd5eaa08a0c8
hunkel <kolan19988@yandex.ru>
2019-07-22 15:07:10 +0300

    refactor checking permissions

--- app/src/main/java/ru/hunkel/mgrouptracker/activities/MainActivity.kt
+++ app/src/main/java/ru/hunkel/mgrouptracker/activities/MainActivity.kt
@@ -1,18 +1,24 @@
 package ru.hunkel.mgrouptracker.activities
 
+import android.Manifest
 import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.content.ServiceConnection
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.os.IBinder
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import ru.hunkel.mgrouptracker.ITrackingService
 import ru.hunkel.mgrouptracker.R
 import ru.hunkel.mgrouptracker.services.TrackingService
+import ru.hunkel.mgrouptracker.utils.enableBluetooth
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -59,14 +65,24 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun startServiceOnClick() {
+        if (acceptPermissions(
+                arrayOf(
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.BLUETOOTH,
+                    Manifest.permission.BLUETOOTH_ADMIN
+                )
+            )
+        ) {
+            enableBluetooth()
 
-        val serviceIntent =
-            Intent(this, TrackingService::class.java)
-        startService(serviceIntent)
-        bindService(serviceIntent, mTrackingServiceConnection, Context.BIND_AUTO_CREATE)
+            val serviceIntent =
+                Intent(this, TrackingService::class.java)
+            startService(serviceIntent)
+            bindService(serviceIntent, mTrackingServiceConnection, Context.BIND_AUTO_CREATE)
 
-        updateUIWithCurrentState(true)
-        mServiceBounded = true
+            updateUIWithCurrentState(true)
+            mServiceBounded = true
+        }
     }
 
     private fun stopServiceOnClick() {
@@ -88,5 +104,31 @@ class MainActivity : AppCompatActivity() {
         if (mServiceBounded) {
             unbindService(mTrackingServiceConnection)
         }
+    }
+
+    private fun acceptPermissions(permissions: Array<String>): Boolean {
+        var grandted = false
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            for (permission in permissions) {
+                grandted = if (
+                    (checkSelfPermission((permission)) != PackageManager.PERMISSION_GRANTED)
+                    and
+                    (shouldShowRequestPermissionRationale(permission))
+                ) {
+                    Toast.makeText(
+                        this,
+                        "Разрешение на местоположение необходимо для записи ваших треков",
+                        Toast.LENGTH_LONG
+                    ).show()
+                    false
+                } else {
+                    true
+                }
+            }
+            requestPermissions(permissions, 1)
+        } else {
+            grandted = true
+        }
+        return grandted
     }
 }
--- app/src/main/java/ru/hunkel/mgrouptracker/utils/Constants.kt
+++ app/src/main/java/ru/hunkel/mgrouptracker/utils/Constants.kt
@@ -1,4 +1,14 @@
 package ru.hunkel.mgrouptracker.utils
 
+import android.bluetooth.BluetoothAdapter
+
 const val STATE_OFF = 0
 const val STATE_ON = 1
+
+fun enableBluetooth(){
+    val mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+    mBluetoothAdapter.cancelDiscovery()
+    if (mBluetoothAdapter.isEnabled.not()) {
+        mBluetoothAdapter.enable()
+    }
+}
