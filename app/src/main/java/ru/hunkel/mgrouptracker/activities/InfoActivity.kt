61c9e03e4cc42c919787f608dcab45846a9fe686
hunkel <kolan19988@yandex.ru>
2019-07-23 19:31:56 +0300

    add info activity

---
+++ app/src/main/java/ru/hunkel/mgrouptracker/activities/InfoActivity.kt
@@ -0,0 +1,13 @@
+package ru.hunkel.mgrouptracker.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.hunkel.mgrouptracker.R
+
+class InfoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_info)
+    }
+}
