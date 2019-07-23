46c218fb661df178ce476bb7248460cee71527e3
hunkel <kolan19988@yandex.ru>
2019-07-24 01:10:48 +0300

    add events and punches list

--- app/src/main/java/ru/hunkel/mgrouptracker/activities/InfoActivity.kt
+++ app/src/main/java/ru/hunkel/mgrouptracker/activities/InfoActivity.kt
@@ -1,10 +1,11 @@
 package ru.hunkel.mgrouptracker.activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_info.*
@@ -12,10 +13,12 @@ import kotlinx.android.synthetic.main.event_list_item.view.*
 import ru.hunkel.mgrouptracker.R
 import ru.hunkel.mgrouptracker.database.entities.Event
 import ru.hunkel.mgrouptracker.database.utils.DatabaseManager
+import ru.hunkel.mgrouptracker.utils.convertLongToTime
 
 class InfoActivity : AppCompatActivity() {
 
     private lateinit var mEventRecyclerView: RecyclerView
+    private lateinit var mDatabaseManager: DatabaseManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,16 +26,17 @@ class InfoActivity : AppCompatActivity() {
 
         mEventRecyclerView = event_recycler_view
         mEventRecyclerView.layoutManager = LinearLayoutManager(this)
-        val dbManager = DatabaseManager(this)
+        mDatabaseManager = DatabaseManager(this)
 
-        val events = dbManager.actionGetAllEvents()
+        val events = mDatabaseManager.actionGetAllEvents()
         mEventRecyclerView.adapter = EventAdapter(events)
     }
 
-    private class EventAdapter(private val eventsList: List<Event>) : RecyclerView.Adapter<EventViewHolder>(){
+    private inner class EventAdapter(private val eventsList: List<Event>) : RecyclerView.Adapter<EventViewHolder>() {
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EventViewHolder {
             val view = LayoutInflater.from(parent.context)
-                .inflate(R.layout.event_list_item,parent,false)
+                .inflate(R.layout.event_list_item, parent, false)
+
             return EventViewHolder(view)
         }
 
@@ -42,12 +46,17 @@ class InfoActivity : AppCompatActivity() {
 
         override fun onBindViewHolder(holder: EventViewHolder, position: Int) {
             holder.bind(eventsList[position])
+            holder.view.setOnClickListener {
+                startActivity(Intent(this@InfoActivity, PunchActivity::class.java).putExtra(KEY_EVENT_ID, position+1))
+            }
         }
     }
 
-    private class EventViewHolder(val view:View) : RecyclerView.ViewHolder(view){
-        fun bind(event: Event){
-            view.event_id_text_view.text = event.id.toString()
+    private class EventViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+        fun bind(event: Event) {
+            view.event_id_text_view.text = "Мероприятие №${event.id}"
+            view.event_start_time_text_view.text = "Время начала: ${convertLongToTime(event.startTime)}"
+            view.event_end_time_text_view.text = "Время окончания: ${convertLongToTime(event.endTime)}"
         }
     }
 }
---
+++ app/src/main/java/ru/hunkel/mgrouptracker/activities/PunchActivity.kt
@@ -0,0 +1,57 @@
+package ru.hunkel.mgrouptracker.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_punch.*
+import kotlinx.android.synthetic.main.event_list_item.view.*
+import kotlinx.android.synthetic.main.punch_list_item.view.*
+import ru.hunkel.mgrouptracker.R
+import ru.hunkel.mgrouptracker.database.entities.Punches
+import ru.hunkel.mgrouptracker.database.utils.DatabaseManager
+const val KEY_EVENT_ID = "keyEvent"
+
+class PunchActivity : AppCompatActivity() {
+
+    private lateinit var mPunchRecyclerView: RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_punch)
+
+        mPunchRecyclerView = punch_recycler_view
+        mPunchRecyclerView.layoutManager = LinearLayoutManager(this)
+        val dbManager = DatabaseManager(this)
+
+        val punchId = intent.getIntExtra(KEY_EVENT_ID,1)
+
+        val punches = dbManager.actionGetPunchesByEventId(punchId)
+        mPunchRecyclerView.adapter = PunchAdapter(punches)
+    }
+
+    private class PunchAdapter(private val punchList: List<Punches>) : RecyclerView.Adapter<PunchViewHolder>(){
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PunchViewHolder {
+            val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.punch_list_item,parent,false)
+            return PunchViewHolder(view)
+        }
+
+        override fun getItemCount(): Int {
+            return punchList.size
+        }
+
+        override fun onBindViewHolder(holder: PunchViewHolder, position: Int) {
+            holder.bind(punchList[position])
+        }
+    }
+
+    private class PunchViewHolder(val view: View) : RecyclerView.ViewHolder(view){
+        fun bind(punch: Punches){
+            view.punch_id_text_view.text = punch.controlPoint.toString()
+        }
+    }
+}
