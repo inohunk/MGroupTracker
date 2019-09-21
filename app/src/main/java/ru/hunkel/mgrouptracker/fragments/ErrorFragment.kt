d345eb09a4378c4275410dffb6c382c930dd6f66
hunkel <kolan19988@yandex.ru>
2019-09-21 18:24:34 +0500

    add error fragment

---
+++ app/src/main/java/ru/hunkel/mgrouptracker/fragments/ErrorFragment.kt
@@ -0,0 +1,36 @@
+package ru.hunkel.mgrouptracker.fragments
+
+
+import android.app.Dialog
+import android.content.DialogInterface
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.DialogFragment
+import kotlinx.android.synthetic.main.fragment_error.view.*
+import ru.hunkel.mgrouptracker.R
+import ru.hunkel.mgrouptracker.utils.ErrorCodes
+
+class ErrorFragment(private val errorCode: Int) : DialogFragment() {
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val builder = AlertDialog.Builder(activity!!)
+        val errorText = ErrorCodes().getErrorByCode(errorCode)
+
+        val view = LayoutInflater.from(context).inflate(R.layout.fragment_error, null)
+        view.message_type_text_view.text = errorText!!.name
+        view.message_text_text_view.text = errorText!!.description
+        builder.setView(view)
+            .setPositiveButton(
+                "Выйти"
+            ) { _, _ ->
+                dismiss()
+                activity?.finish()
+            }
+        return builder.create()
+    }
+
+    override fun onCancel(dialog: DialogInterface) {
+        activity!!.finish()
+    }
+}
